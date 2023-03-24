48363a4cf6c1ab660ef6735f317038297f2b7052
libni1923 <libnipinto10@gmail.com>
2023-03-23 22:39:02 -0600

    se cambia a hola mundo

---
+++ practica para examen.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i = 0;
+	cout<<"\Ingrese un numero del uno al 10"<<endl;
+	cin>>i;
+	while(i<=10){
+		i = i+1;
+		cout<<" "<<i;
+	}
+	
+}
